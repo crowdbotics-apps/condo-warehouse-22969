927709571cc13431fc15e0e296ec4675f1ebda72
Crowdbotics <team@crowdbotics.com>
2020-11-25 06:21:33 +0000

    Update models

--- condo_warehouse_22969/wsgi.py
+++ condo_warehouse_22969/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'condo_warehouse_22969.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "condo_warehouse_22969.settings")
 
 application = get_wsgi_application()
